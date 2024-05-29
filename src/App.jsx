a03e61d1e0f15309612a599d172ce926fc9ecbf8
Shashank Sharma <shashanksharma8868@gmail.com>
2024-05-29 20:49:45 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Topics Covered</h1>
+    <div className="content">
+      <a href="#">Getting started with the web</a>
+      <p>Provides a practical introduction to the web develpoment for beginners</p>
+      <a href="">HTML- Structuring the web</a>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, nam eligendi! Id sunt, deserunt rerum rem delectus ipsa, voluptatum asperiores impedit sapiente neque dolor soluta ipsam velit! Iste, autem iusto.ṣ</p>
+      <a href=""> CSS- Styling the web</a>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam quam aspernatur dignissimos commodi neque natus exercitationem facilis rerum inventore est minima excepturi numquam autem, impedit eius ab asperiores maxime soluta!</p>
+    </div>
+ 
+    </>
+  )
+}
+
+export default App
